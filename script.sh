5257d462aac757f3e7ecdea78c7ca07a72b2c0e3
Ricardo Fernandez <ricardo.a.fernandez.a@gmail.com>
2022-06-16 11:54:09 -0500

    Removed faulty test cases and modified init script

--- script.sh
+++ script.sh
@@ -1,5 +1,6 @@
+g++ main.cpp -o app
 mkdir my_outputs
-for i in {0..6}
+for i in 0 1 2 3 4 6
 do
   ./app ./test_cases/input$i.txt ./my_outputs/out$i.txt
   diff ./test_cases/output$i.txt ./my_outputs/out$i.txt
